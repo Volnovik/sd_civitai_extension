2075aee2cd1cdb236add73242afe4064bc4fb6ad
Volnovik <75752255+Volnovik@users.noreply.github.com>
2025-12-09 20:11:54 +0300

    Update pasted.py
    deleted shared.opts.disable_weights_auto_swap, does nothing for Forge

--- scripts/pasted.py
+++ scripts/pasted.py
@@ -5,7 +5,7 @@ from modules import shared, script_callbacks
 # Automatically pull model with corresponding hash from Civitai
 def on_infotext_pasted(infotext, params):
     download_missing_models = shared.opts.data.get('civitai_download_missing_models', True)
-    if (not download_missing_models or "Model hash" not in params or shared.opts.disable_weights_auto_swap):
+    if (not download_missing_models or "Model hash" not in params):
         return
 
     model_hash = params["Model hash"]
